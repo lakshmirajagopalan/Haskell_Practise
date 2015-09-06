1b6fc473e589748347d383dcc7f55a323ccaf0c2
lakshmirajagopalan <lakshmi.srajagopalan@gmail.com>
2015-09-06 18:56:04 +0530

    Arithmetic expr parser

--- Chapters/ch_8.hs
+++ Chapters/ch_8.hs
@@ -125,3 +125,36 @@ naturalNumberList = do
 					 			natural)
 					symbol "]"
 					return (v:vs)
+
+
+expr :: Parser Int					
+expr = do
+		t <- term
+		do 
+			symbol "+" 
+			e <- expr
+			return (t + e)
+			+++ return t
+
+term ::	Parser Int
+term = do
+		f <- factor
+		do 
+			symbol "*"	
+			t <- term
+			return (f * t)
+			+++ return f	
+
+factor :: Parser Int
+factor = do 
+		 	symbol "("
+		 	e <- expr
+		 	symbol ")"
+			return e
+			+++ natural
+
+eval :: String -> Int
+eval input = case parse (expr) input of
+				[] -> error "invalid input"
+				[(a, [])] -> a
+				[(a, out)] -> error "cannot complete parsing"
